2bf93153d115a889ef1c9457e38e0e6f0e648c17
edik-Chimittsydenov <edik.chimittsydenov@gmail.com>
2024-11-24 19:16:33 +0300

    Created function exchange for get a current from internet

---
+++ Obmen.py
@@ -0,0 +1,11 @@
+import requests
+import json
+import pprint
+
+
+result = requests.get("https://open.er-api.com/v6/latest/USD")
+data = json.loads(result.text)
+#print(data)
+p=pprint.PrettyPrinter(indent=4)
+
+p.pprint(data)
